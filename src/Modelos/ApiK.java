3789ff60ac4afa4d0a29ea242ca74d46c8100850
La chiva7 <miguel19j@gmail.com>
2024-05-09 20:06:20 -0400

    cambios para ocultar mi llave

---
+++ src/Modelos/ApiK.java
@@ -0,0 +1,5 @@
+package Modelos;
+
+public interface ApiK {
+     static final String KEY_FINAL = "8f13be19f71b11d054d1dc5b";
+}
--- src/Services/ConsultaAPIMoneda.java
+++ src/Services/ConsultaAPIMoneda.java
@@ -1,5 +1,6 @@
 package Services;
 
+import Modelos.ApiK;
 import Modelos.Conversor;
 import com.google.gson.Gson;
 
@@ -8,9 +9,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class ConsultaAPIMoneda {
+public class ConsultaAPIMoneda implements ApiK {
     public Conversor buscaMoneda(String denominacionBase){
-        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/b567e9d7a6af531af245c9c3/latest/"+denominacionBase);
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/"+KEY_FINAL+"/latest/"+denominacionBase);
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(direccion)
